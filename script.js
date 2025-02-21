0ca6a2c02cb4687aa829c77d22412d01cbe14a6f
shimjun1 <desmondshim1@gmail.com>
2025-02-21 23:06:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('confessButton').addEventListener('click', function() {
+    const response = document.getElementById('response');
+    response.textContent = "我希望你的答案是“愿意”！💖";
+});
